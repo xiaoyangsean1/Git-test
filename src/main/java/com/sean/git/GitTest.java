ec98be9a65b383b574150111399654ee027aac92
Sean <xiaoyangsean@126.com>
2022-10-18 08:48:00 +0800

    first commit

---
+++ src/main/java/com/sean/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.sean.git;
+
+/**
+ * @小羊肖恩
+ * @2022/10/18
+ * @8:43
+ * @Describe：
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
